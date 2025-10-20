6743aa4c3f308fed7498fa1e8c89d8fc0d74f273
Abrar <rmd394195@gmial.comn>
2025-10-20 15:19:45 +0600

    problem

---
+++ multipul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+   long long int A,B;
+    scanf("%lld%lld",&A,&B);
+    printf("%lld",A*B);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
---
+++ problemH.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    if(N%3==0){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
